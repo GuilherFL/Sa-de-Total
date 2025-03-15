a80f5e372440ce6927b84ab2c5b71782fcbf54b5
GuilhermeFL <163077062+GuilherFL@users.noreply.github.com>
2025-03-14 21:45:03 -0300

    Add files via upload

---
+++ static/webpack/app/bookings/page.8b75a40ac3c78109.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/bookings/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cguilh%5COneDrive%5C%C3%81rea%20de%20Trabalho%5CPI%5CBARBERBLEND-main%5Capp%5C_components%5Cheader.tsx&server=false!":
+/*!************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cguilh%5COneDrive%5C%C3%81rea%20de%20Trabalho%5CPI%5CBARBERBLEND-main%5Capp%5C_components%5Cheader.tsx&server=false! ***!
+  \************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/_components/header.tsx */ \"(app-pages-browser)/./app/_components/header.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1DJTNBJTVDVXNlcnMlNUNndWlsaCU1Q09uZURyaXZlJTVDJUMzJTgxcmVhJTIwZGUlMjBUcmFiYWxobyU1Q1BJJTVDQkFSQkVSQkxFTkQtbWFpbiU1Q2FwcCU1Q19jb21wb25lbnRzJTVDaGVhZGVyLnRzeCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ZWRlNSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkM6XFxcXFVzZXJzXFxcXGd1aWxoXFxcXE9uZURyaXZlXFxcXMOBcmVhIGRlIFRyYWJhbGhvXFxcXFBJXFxcXEJBUkJFUkJMRU5ELW1haW5cXFxcYXBwXFxcXF9jb21wb25lbnRzXFxcXGhlYWRlci50c3hcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cguilh%5COneDrive%5C%C3%81rea%20de%20Trabalho%5CPI%5CBARBERBLEND-main%5Capp%5C_components%5Cheader.tsx&server=false!\n"));
+
+/***/ })
+
+});
